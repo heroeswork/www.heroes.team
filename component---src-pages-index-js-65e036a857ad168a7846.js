0a5f1cb8fdb9c84ce7030e598cb9ce508f9a8695
Alan Gardner <me@alangardner.net>
2019-06-16 16:50:29 -0700

    Updates

---
+++ component---src-pages-index-js-65e036a857ad168a7846.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{157:function(e,n,t){"use strict";t.r(n);var a=t(0),i=t.n(a);n.default=function(){return i.a.createElement("div",null,i.a.createElement("h1",null,"Heroes Initiative, Inc."),i.a.createElement("p",null,"Coming soon..."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-65e036a857ad168a7846.js.map
