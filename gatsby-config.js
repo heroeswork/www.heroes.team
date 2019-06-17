8bf9a97fc8f59ae87bce6e6c04bfa77931106b58
Alan Gardner <me@alangardner.net>
2019-06-17 13:39:40 -0700

    Add Google Analytics to page

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,29 @@ module.exports = {
         icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-137203731-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        //anonymize: true,
+        // Setting this parameter is also optional
+        //respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        //exclude: [],
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
+        //cookieDomain: "example.com",
+      },
+    },
   ],
 }
