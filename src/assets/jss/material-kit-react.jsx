9c4ead78a6a2b301d7e0c74ce15966a434edf06c
Alan Gardner <me@alangardner.net>
2019-06-17 13:07:52 -0700

    Update fonts to nunito open-sans

--- src/assets/jss/material-kit-react.jsx
+++ src/assets/jss/material-kit-react.jsx
@@ -64,7 +64,7 @@ const card = {
 };
 
 const defaultFont = {
-  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  fontFamily: '"Nunito", "Open Sans", "Arial", sans-serif',
   fontWeight: "300",
   lineHeight: "1.5em"
 };
@@ -159,8 +159,8 @@ const title = {
   color: "#3C4858",
   margin: "1.75rem 0 0.875rem",
   textDecoration: "none",
-  fontWeight: "700",
-  fontFamily: `"Roboto Slab", "Times New Roman", serif`
+  fontWeight: "800",
+  fontFamily: '"Nunito", "Open Sans", "Arial", sans-serif'
 };
 
 const cardTitle = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import {createMemoryHistory} from "history";
 import {Route, Router, Switch} from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
-import 'typeface-roboto';
-import 'typeface-roboto-slab';
+import 'typeface-nunito';
+import 'typeface-open-sans';
 
 // pages for this site
 import LandingPage from "./LandingPage/LandingPage.jsx";
