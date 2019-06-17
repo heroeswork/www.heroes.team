a6b6ac2de706ac4780d82668930c06b377424871
Alan Gardner <me@alangardner.net>
2019-06-17 12:21:49 -0700

    Add Crisp chat functionality for contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,27 @@ import LandingPage from "./LandingPage/LandingPage.jsx";
 
 let hist = createMemoryHistory();
 
-export default () => (
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={LandingPage} />
-    </Switch>
-  </Router>
-);
+class App extends React.Component {
+  componentDidMount () {
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "cc8dca6c-1c5c-4505-a126-3cb1e3462a36";
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  }
+
+  render () {
+    return (
+      <Router history={hist}>
+        <Switch>
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>)
+  }
+}
+export default App
