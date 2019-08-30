eb1cf92134e270044898404866d5aa3319fff35a
Alan Gardner <me@alangardner.net>
2019-08-30 10:48:55 -0700

    Better skyline parallax flow

--- assets/scripts/vendor/jquery-parallax.js
+++ assets/scripts/vendor/jquery-parallax.js
@@ -10,7 +10,7 @@ import "jquery-parallax";
 
   $(document).ready(function(){
     if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-      $('.js-parallax').parallax("50%", .3);
+      $('.js-parallax').parallax("50%", .2);
     }
   })
 
