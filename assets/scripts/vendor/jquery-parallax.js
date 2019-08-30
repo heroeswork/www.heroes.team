e1c5bd9016752f6a418e4f06ad0b63adf06e375e
Alan Gardner <me@alangardner.net>
2019-08-30 12:35:14 -0700

    Show parallax effects on mobile

--- assets/scripts/vendor/jquery-parallax.js
+++ assets/scripts/vendor/jquery-parallax.js
@@ -9,7 +9,7 @@ import "jquery-parallax";
   'use strict';
 
   $(document).ready(function(){
-    if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+    if (!(/iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
       $('.js-parallax').parallax("50%", .2);
     }
   })
