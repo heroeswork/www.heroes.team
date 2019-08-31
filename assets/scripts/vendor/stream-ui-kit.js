adda3aface36bd2f4d9fbef089dd7388d0a10e48
Alan Gardner <me@alangardner.net>
2019-08-30 17:16:21 -0700

    Increase scroll height for header color change

--- assets/scripts/vendor/stream-ui-kit.js
+++ assets/scripts/vendor/stream-ui-kit.js
@@ -21,13 +21,13 @@
   // Bootstrap Fixed Header
   $(function() {
     // Check to see if there is a bakcground class on loading
-    if ($('.js-navbar-scroll').offset().top > 150) {
+    if ($('.js-navbar-scroll').offset().top > 300) {
       $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
     }
 
     // Check to add a background class on scrolling
     $(window).on('scroll', function() {
-      var navbarOffset = $('.js-navbar-scroll').offset().top > 150;
+      var navbarOffset = $('.js-navbar-scroll').offset().top > 300;
       if(navbarOffset) {
         $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
       }
