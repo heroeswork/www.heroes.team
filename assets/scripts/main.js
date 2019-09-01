e26746b4c7e61dd7665652e7d1bf3f95720c6465
Alan Gardner <me@alangardner.net>
2019-09-01 14:01:12 -0700

    Add chat and schedule to contact section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 // Vendors
 import './vendor/jquery'
 import './vendor/bootstrap'
+import './vendor/crisp-chat'
 import './vendor/fontawesome'
 import './vendor/jquery-parallax'
 import './vendor/stream-ui-kit'
--- assets/scripts/vendor/stream-ui-kit.js
+++ assets/scripts/vendor/stream-ui-kit.js
@@ -37,20 +37,4 @@
       }
     });
   });
-
-  // Scroll to (Section)
-  $(function() {
-    $('a[href*=#js-scroll-to-]:not([href=#js-scroll-to-])').on('click', function() {
-      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top - 10
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
 }(jQuery));
